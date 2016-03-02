606dca25ff280bf395195aff68f13f3749b4d861
tommyre <tommyre@gmail.com>
2016-03-02 03:37:53 +0100

    update

--- simpleCards/javascripts/app.js
+++ simpleCards/javascripts/app.js
@@ -21,46 +21,101 @@
 
         }
 
+        var aiScore = 0;
+        var playerScore = 0;
+
     function drawRandomCard() {
         return deck[Math.floor(Math.random() * deck.length)];
     }
 
-    function drawCard(nmbOfCards) {
 
-            var cardCanvas = document.getElementById("cardCanvas");
-            var context = cardCanvas.getContext("2d");
+    function drawCard(cardSlot) {
 
-            for (i = 0; i < nmbOfCards; i++) {
-                if (context) {
-                    var xPlacement = 20;
-                    var xCards = 1;
+        var xPlace = 20;
+        var yPlace = 20;
 
-                    if (xCards > 1) {
-                        xPlacement = (160 * xCards) + 10
+        switch (cardSlot) {
+
+            case 1:
+                xPlace = 20;
+                break;
+            case 2:
+                xPlace = 190;
+                break;
+            case 3:
+                xPlace = 20;
+                yPlace = 240;
+                break;
+            case 4:
+                xPlace = 190;
+                yPlace = 240;
+                break;
+        }
+
+        var cardCanvas = document.getElementById("cardCanvas");
+        var context = cardCanvas.getContext("2d");
+
+            if (context) {
+
+                var currentCard = drawRandomCard();
+                if (currentCard) {
+                    var currentImage = new Image();
+
+                    currentImage.onload = function () {
+
+
+                        context.drawImage(this, xPlace, yPlace, 150, 200);
+
+                    };
+                    currentImage.src = currentCard.img;
+                    
+                    //Creates cardSlotValue variables for holding the value of the card. Set value based on rules of blackjack.
+                    if (currentCard.cardvalue > 10 && currentCard.cardvalue < 14) {
+                        window['cardSlotValue' + cardSlot] = 10;
+                    }
+                    else if (currentCard.cardvalue == 14) {
+                        window['cardSlotValue' + cardSlot] = 11;
+                    }
+                    else {
+                        window['cardSlotValue' + cardSlot] = currentCard.cardvalue;
                     }
 
-                    var currentCard = drawRandomCard();
-                    if (currentCard) {
-                        var currentImage = new Image();
-
-                        currentImage.onload = function () {
-
-                            context.drawImage(this, xPlacement, 20, 150, 200);
-
-                        };
-
-                        currentImage.src = currentCard.img;
+                    //Remove card from deck array, so it can't be drawn again
+                    var index = deck.indexOf(currentCard);
+                    if (index > -1) {
+                        deck.splice(index, 1);
                     }
+
                 }
-                xCards++;
             }
+
     }
 
+    function calculateScore() {
+        aiScore = cardSlotValue1 + cardSlotValue2;
+        playerScore = cardSlotValue3 + cardSlotValue4;
+    }
 
+    
+    function drawValue() {
+        var c = document.getElementById("cardCanvas");
+        var ctx = c.getContext("2d");
+
+        ctx.font = "20px Georgia";
+        //clears a rectagle of the canvas so the text doesn't write over it.
+        ctx.clearRect(460, 0, 50, 500);
+        ctx.fillText(aiScore, 460, 105);
+        ctx.fillText(playerScore, 460, 340);
+    }
 
     $("#btnDraw").on("click", function () {
 
+        drawCard(1);
         drawCard(2);
+        drawCard(3);
+        drawCard(4);
+        calculateScore();
+        drawValue();
 
     });
 
